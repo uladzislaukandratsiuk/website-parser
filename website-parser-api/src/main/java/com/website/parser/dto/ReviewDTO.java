4135b739c0ecde2a4703b24d6a3b7700c86e80c8
uladzislau_kandratsiuk <vladislavkondratuk322@gmail.com>
2022-10-13 17:14:15 +0300

    add dockerfile and readme: add dockerfile and readme

--- website-parser-api/src/main/java/com/website/parser/dto/ReviewDTO.java
+++ website-parser-api/src/main/java/com/website/parser/dto/ReviewDTO.java
@@ -3,10 +3,12 @@ package com.website.parser.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Builder
+@ToString
 public class ReviewDTO {
 
     private String reviewsCount;
